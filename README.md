0a36f96aee9dc8d98b5254ee860cedb10015e4d1
Carlos <cl5045816@gmail.com>
2026-09-18 20:41:21 -0300

    git atualizado

--- README.md
+++ README.md
@@ -1,3 +1,2 @@
 # JogoMario
 Joguinho do Mario 🌟🍄
-
--- script.js
+++ script.js
@@ -9,7 +9,15 @@ const jump = () => {
     setTimeout(() => {
         mario.classList.remove('jump');
     }, 1500);
+}
+const loop = setInterval (() => {
+const pipePosition = pipe.offsetLeft;
 
+if(pipePosition <= 120){
+    pipe.style.animation = 'none';
+    pipe.style.left = `${pipePosition}px`
 }
 
+},10);
 document.addEventListener('keydown', jump);
+
